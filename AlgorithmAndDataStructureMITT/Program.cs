0dc5ffab7bf645d4408dea63a29f5729d6a590b1
Elochukwu Aroh <elochukwuaroh@gmail.com>
2022-06-12 04:45:25 -0500

    lab 2 submission initial commit, incomplete code

--- AlgorithmAndDataStructureMITT/Program.cs
+++ AlgorithmAndDataStructureMITT/Program.cs
@@ -1,48 +1,66 @@
-﻿string[] wordsArray = { };
-int wordsNumber;
-char userChar;
-char[] userCharsArray = { };
-
-Console.WriteLine("Please input the number of words for this array");
-wordsNumber = Int32.Parse(Console.ReadLine());
-
-for (int index = 1; index <= wordsNumber; index++)
+﻿void recieveUserInput()
 {
-    Console.WriteLine($"Please enter word {index}");
-    string word = Console.ReadLine();
-    Array.Resize(ref wordsArray, wordsArray.Length + 1);
-    wordsArray[wordsArray.Length - 1] = word;
+    int wordsNumber;
+    string[] wordsArray = { };
+    char userChar;
+    char[] userCharsArray = { };
 
-}
+    Console.WriteLine("Please input the number of words for this array");
+    wordsNumber = Int32.Parse(Console.ReadLine());
 
-Console.WriteLine("Please enter a character to compare");
-userChar = Console.ReadKey().KeyChar;
-
-foreach (string word in wordsArray)
-{
-    int charCount = 0;
-
-    userCharsArray = word.ToCharArray(0, word.Length);
-    for (int index = 0; index < userCharsArray.Length; index++)
+    for (int index = 1; index <= wordsNumber; index++)
     {
-        if (userCharsArray[index] == userChar)
+        Console.WriteLine($"Please enter word {index}");
+        string word = Console.ReadLine();
+        Array.Resize(ref wordsArray, wordsArray.Length + 1);
+        wordsArray[wordsArray.Length - 1] = word;
+
+    }
+
+    Console.WriteLine("Please enter a character to compare");
+    userChar = Console.ReadKey().KeyChar;
+
+
+    //Look here
+    foreach (string word in wordsArray)
+    {
+        int charCount = 0;
+
+        userCharsArray = word.ToCharArray(0, word.Length);
+        for (int index = 0; index < userCharsArray.Length; index++)
         {
-            charCount++;
+            if (userCharsArray[index] == userChar)
+            {
+                charCount++;
+            }
         }
-    }
-    Console.WriteLine($"charater count {charCount}, word length {userCharsArray.Length}");
-    double charInWordPercentage;
-  
-    charInWordPercentage = charCount * 100 / userCharsArray.Length ;
-    Console.WriteLine(charInWordPercentage);
+        Console.WriteLine($"charater count {charCount}, word length {userCharsArray.Length}");
+        double charInWordPercentage;
 
-    if (charInWordPercentage >= 25)
-    {
-        Console.WriteLine($"The letter '{userChar}' appeared in {word} {charCount} times. This letter makes up more than 25% of the total number of characters.  ");
-    }
-    else if (charInWordPercentage < 25)
-    {
-        Console.WriteLine($"The letter '{userChar}' appeared in {word} {charCount} times.");
+        charInWordPercentage = charCount * 100 / userCharsArray.Length;
+        Console.WriteLine(charInWordPercentage);
+
+        if (charInWordPercentage >= 25)
+        {
+            Console.WriteLine($"The letter '{userChar}' appeared in {word} {charCount} times. This letter makes up more than 25% of the total number of characters.  ");
+        }
+        else if (charInWordPercentage < 25)
+        {
+            Console.WriteLine($"The letter '{userChar}' appeared in {word} {charCount} times.");
+        }
+
     }
 
 }
+
+
+string getLetterOccurenceString(char letter, string word)
+{
+
+    return "csbhfiwefaenfi";
+}
+
+
+
+
+
